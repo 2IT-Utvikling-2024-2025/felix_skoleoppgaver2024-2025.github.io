4ddd0bcdb1f4fded611f4fd22b3c10086c8d18f9
Glafyr <felixborrm@gmail.com>
2024-08-28 15:33:12 +0200

    Added clock

---
+++ Oppgave2_react/digitalclock/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+
+const App = () => {
+
+    const [time, setTime] = useState(Date.now())
+
+    setInterval(() => {
+        setTime(Date.now())
+    }, 1000)
+
+    return (
+        <main className='w-screen h-screen flex items-center justify-center bg-[#110119] flex-col'>
+            <h1 className='text-5xl text-white font-bold'>Digital Clock</h1>
+            <div className='mb-10 mt-4 w-96 h-1 bg-gradient-to-r from-blue-700 via-purple-700 to-pink-500 '></div>
+            <h1 className='text-5xl font-bold bg-gradient-to-r from-blue-700 via-purple-700 to-pink-500 bg-clip-text text-transparent p-10 border-[1px] rounded-lg shadow-sm border-purple-500 '>{new Date(time).toLocaleTimeString()}</h1>
+        </main>
+    )
+}
+
+export default App
